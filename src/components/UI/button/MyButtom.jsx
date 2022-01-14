268308896a7fed612368f522798f0528dc16c949
RomanNikitenko <68384950+RomanNikitenko19@users.noreply.github.com>
2022-01-14 16:23:26 +0200

    button and title

---
+++ src/components/UI/button/MyButtom.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from "./MyButton.module.css";
+
+const MyButton = ({ children, isActiv = false,  ...props}) => {
+  return (
+    <button {...props} className={isActiv ? (classes.myActivBtn, classes.myBtn ) : classes.myBtn}>
+      {children}
+    </button>
+  );
+};
+
+export default MyButton;
