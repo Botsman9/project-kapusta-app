7c1418ca473082a6eb1e7aa983533c8c491323cf
Dmitriy <dmitriy.botsman@gmail.com>
2022-01-15 11:56:05 +0200

    balance/marcap/redux

---
+++ src/components/Balance/Balance.jsx
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import s from './Balance.module.css';
+import { getTotalBalance } from 'redux/transactions/transactions-selectors';
+
+import ModalBalance from '../modalBalance/modalBalance';
+
+const Balance = ({ hide, width }) => {
+  const balance = useSelector(getTotalBalance);
+  const dispatch = useDispatch();
+  const [sum, setSum] = useState('');
+  const [setPromptClose, setClosePrompt] = useState(true);
+  const toggleWindow = () => {
+    setClosePrompt(setClosePrompt => !setClosePrompt);
+  };
+
+  const onHandleChange = e => setSum(e.currentTarget.value);
+  useEffect(() => {
+    setSum(balance);
+  }, [balance]);
+
+  const onhandleSubmit = e => {
+    e.preventDefault();
+  };
+  return (
+    <form onSubmit={onhandleSubmit} className={s.reportBalance}>
+      <label htmlFor="balans" className={s.balanceLabel}>
+        Баланс:
+        <div className={s.buttonsGroup}>
+          {balance === 0 ? (
+            <>
+              {setPromptClose && <ModalBalance onClose={toggleWindow} />}
+              <input
+                type="text"
+                name="name"
+                maxLength="10"
+                placeholder="00.00"
+                onChange={onHandleChange}
+                className={
+                  width
+                    ? `${s.balanceInputReport} ${s.balanceInput}`
+                    : `${s.balanceInput}`
+                }
+                // className={s.balanceInput}
+                autoComplete="off"
+              />
+              <button
+                className={
+                  width
+                    ? `${s.balanceInputReport} ${s.balanceButton}`
+                    : `${s.balanceButton} `
+                }
+                type="submit"
+              >
+                ПОДТВЕРДИТЬ
+              </button>
+            </>
+          ) : (
+            <>
+              <p
+                className={
+                  width
+                    ? `${s.balanceInput} ${s.balanceInputReport}`
+                    : `${s.balanceInput}`
+                }
+              >
+                {`${balance.toLocaleString('ru')}.00`} UAH
+              </p>
+              <button className={`${s.balanceButton} ${hide}`} disabled>
+                ПОДТВЕРДИТЬ
+              </button>
+            </>
+          )}
+        </div>
+      </label>
+    </form>
+  );
+};
+export default Balance;
