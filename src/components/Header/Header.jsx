c4b4f169c26ce01a90fa90b94703d5a64f319ae1
RomanNikitenko <68384950+RomanNikitenko19@users.noreply.github.com>
2022-01-17 12:52:45 +0200

    fix menu logout in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,4 @@
+import { useSelector } from 'react-redux';
 import logo from '../../imges/svg/logo.svg';
 import s from './Header.module.css';
 import UserMenuMob from './UserMenu/UserMenuMob';
@@ -5,11 +6,12 @@ import UserMenu from './UserMenu/UserMenu';
 import useWindowSize from '../../hooks/useWindowSize';
 
 function Header() {
+  const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
   const [height, width] = useWindowSize();
   return (
     <div className={s.HeaderContainer}>
       <img src={logo} alt="logo" className={s.logo} />
-      {width <= 787 ? <UserMenuMob /> : <UserMenu />}
+      {isLoggedIn && (width <= 787 ? <UserMenuMob /> : <UserMenu />)}
     </div>
   );
 }
