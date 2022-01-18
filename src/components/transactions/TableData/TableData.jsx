ef57f4fd3817ad5e62f0dcfdb72ed57f91f8802e
Dmitriy <dmitriy.botsman@gmail.com>
2022-01-18 18:41:25 +0200

    fix-css

--- src/components/transactions/TableData/TableData.jsx
+++ src/components/transactions/TableData/TableData.jsx
@@ -4,42 +4,52 @@ import s from './TableData.module.css';
 
 const TableData = ({ dataTransactions, onChangeDel, isExpense = false }) => {
   return (
-    <table className={s.table}>
-      <thead>
-        <tr>
-          <th className={s.titleLeft}>Дата</th>
-          <th>Описание</th>
-          <th>Категория</th>
-          <th>Сумма</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        {dataTransactions.map(
-          ({ _id, date, description, category, amount }) => (
-            <tr key={_id}>
-              <td>{normalizeDateRender(date)}</td>
-              <td>{description}</td>
-              <td className={s.center}>{category}</td>
-              <td className={isExpense ? s.expenseText : s.incomeText}>
-                {isExpense ? '- ' : ''}
-                {amount}.00 грн.
-              </td>
-              <td>
-                <button onClick={() => onChangeDel(_id)}>del</button>
-              </td>
-            </tr>
-          ),
-        )}
-        {/* <tr key="tests_325626ss">
+    <div className={s.bodyTable}>
+      {' '}
+      <table className={s.main}>
+        <thead className={s.theadTable}>
+          <tr>
+            <th className={s.titleLeft}>Дата</th>
+            <th className={`${s.th} ${s.thData}`}>Описание</th>
+            <th className={`${s.th} ${s.thData}`}>Категория</th>
+            <th className={`${s.th} ${s.thData}`}>Сумма</th>
+            <th className={`${s.th} ${s.thData}`}></th>
+          </tr>
+        </thead>
+      </table>
+      <div className={s.bodyTableScroll}>
+        <tbody>
+          {dataTransactions.map(
+            ({ _id, date, description, category, amount }) => (
+              <tr key={_id} className={s.td}>
+                <td className={s.thData}>{normalizeDateRender(date)}</td>
+                <td className={s.tdDesc}>{description}</td>
+                <td className={s.thCateg}>{category}</td>
+                <td className={isExpense ? s.tdSum : s.tdSumExpense}>
+                  {isExpense ? '- ' : ''}
+                  {amount}.00 грн.
+                </td>
+                <td className={s.thIcon}>
+                  <button
+                    lassName={s.deleteBtn}
+                    onClick={() => onChangeDel(_id)}
+                  >
+                    del
+                  </button>
+                </td>
+              </tr>
+            ),
+          )}
+          {/* <tr key="tests_325626ss">
           <td>05.09.2019</td>
           <td>Метро</td>
           <td>Транспорт</td>
           <td>- 30.00 грн.</td>
 
         </tr> */}
-      </tbody>
-    </table>
+        </tbody>
+      </div>
+    </div>
   );
 };
 
