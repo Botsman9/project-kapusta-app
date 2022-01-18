417b88fbb846f0fa381548b22086f5ba6867a4c3
Kuliiev Roman <Mejas.black@gmail.com>
2022-01-18 11:40:37 +0200

    fixed

--- src/pages/TransactionsPage/TransactionsPage.jsx
+++ src/pages/TransactionsPage/TransactionsPage.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
 import { Outlet } from 'react-router-dom';
 import TransactionsNav from '../../components/transactions/TransactionsNav/TransactionsNav';
-import { fetchAllStatistics } from '../../redux/statistics/statisticsOperations';
 import s from './TransactionsPage.module.css';
 
 const TransactionsPage = () => {
-  const dispatch = useDispatch();
-  dispatch(fetchAllStatistics('2022-01'));
   return (
     <div>
       Страница содержащая в себе доходы и расходы
