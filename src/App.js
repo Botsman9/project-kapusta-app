d032a154f11eecf3d93950abd3ecd1ec5de4ec8e
MaksVibe <parunovmaksym@gmail.com>
2022-01-18 17:53:55 +0200

    fix all refresh logic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Header from './components/Header/Header';
 import AllRoutes from './routes/AllRoutes.jsx';
@@ -7,23 +7,19 @@ import operations from './redux/auth/auth-operartions';
 import BackgroundCont from './components/BackgroundCont/BackgroundCont';
 import BackgroundLogin from './components/BackgroundLogin/BackgroundLogin';
 import Container from './components/Container/Container';
+import { getSid, getIsLoggedIn } from './redux/auth/auth-selectors';
 
 function App() {
   const dispatch = useDispatch();
-  const sid = useSelector(state => state.auth.sid);
-  const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+  const isLoggedIn = useSelector(getIsLoggedIn);
 
   useEffect(() => {
-    if (isLoggedIn) {
-      dispatch(userOperations.getAllUserInfo());
-    }
-  }, [dispatch, isLoggedIn]);
+    dispatch(userOperations.getAllUserInfo());
+  }, [dispatch]);
 
   useEffect(() => {
-    if (sid) {
-      dispatch(operations.refresh({ sid }));
-    }
-  }, [dispatch, sid]);
+    dispatch(operations.refresh());
+  }, [dispatch]);
 
   return (
     <>
