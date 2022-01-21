573f0042e74735a10e5bfd5686725184bc274160
MaksVibe <parunovmaksym@gmail.com>
2022-01-21 14:42:12 +0200

    Add write Refresh, catch errors, edit auth-slice

--- src/redux/auth/auth-selectors.js
+++ src/redux/auth/auth-selectors.js
@@ -1,2 +1,3 @@
 export const getSid = state => state.auth.sid;
 export const getIsLoggedIn = state => state.auth.isLoggedIn;
+export const getRefreshToken = state => state.auth.refreshToken;
--- src/redux/auth/auth-slise.js
+++ src/redux/auth/auth-slise.js
@@ -25,33 +25,56 @@ const authSlice = createSlice({
     },
   },
   extraReducers: {
+    [authOperations.register.pending](state) {
+      state.isRefresh = true;
+      state.error = null;
+    },
     [authOperations.register.fulfilled](state, { payload }) {
       state.token = payload.accessToken;
       state.sid = payload.sid;
       state.isLoggedIn = true;
-    },
-    [authOperations.register.pending](state) {
-      state.error = null;
+      state.isRefresh = false;
     },
     [authOperations.register.rejected](state, { payload }) {
       state.error = payload;
+      state.isRefresh = false;
+      state.isLoggedIn = false;
     },
 
+    [authOperations.logIn.pending](state) {
+      state.isRefresh = true;
+      state.error = null;
+    },
     [authOperations.logIn.fulfilled](state, { payload }) {
       state.token = payload.accessToken;
       state.sid = payload.sid;
       state.isLoggedIn = true;
+      state.isRefresh = false;
+    },
+    [authOperations.logIn.rejected](state, { payload }) {
+      state.error = payload;
+      state.isRefresh = false;
     },
 
+    [authOperations.logOut.pending](state, action) {
+      state.isRefresh = true;
+      state.error = null;
+    },
     [authOperations.logOut.fulfilled](state, action) {
       state.token = null;
       state.refreshToken = null;
       state.sid = null;
       state.isLoggedIn = false;
+      state.isRefresh = false;
+    },
+    [authOperations.logOut.rejected](state, { payload }) {
+      state.error = payload;
+      state.isRefresh = false;
     },
 
     [authOperations.refresh.pending](state, action) {
       state.isRefresh = true;
+      state.error = null;
     },
     [authOperations.refresh.fulfilled](state, { payload }) {
       state.token = payload.data.newAccessToken;
@@ -60,7 +83,8 @@ const authSlice = createSlice({
       state.isLoggedIn = true;
       state.isRefresh = false;
     },
-    [authOperations.refresh.rejected](state) {
+    [authOperations.refresh.rejected](state, payload) {
+      state.error = payload;
       state.isRefresh = false;
     },
   },
